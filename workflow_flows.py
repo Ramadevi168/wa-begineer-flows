ec29362208a71632c593d81baaa7a89cfaf92f15
Ramadevi168 <samula.ramadevi@ibm.com>
2025-12-15 17:05:29 +0530

    Update workflow_flows.py

--- workflow_flows.py
+++ workflow_flows.py
@@ -1,36 +1,34 @@
 from prefect import flow, task
-
+ 
 @task
 def extract():
     return "Data Extracted"
-
+ 
 @task
 def transform(data):
     return f"{data} → Transformed"
-
+ 
 @task
 def validate(data):
     return f"{data} → Validated"
-
+ 
 @task
 def load(data):
     return f"{data}"  # Logs will not show in Cloud
-
-
+ 
 @flow
 def etl_flow(job_name: str = "Daily ETL"):
     raw = extract()
-	processed = transform(raw)
+    processed = transform(raw)
     validated = validate(processed)
-	load(validated)
-
-	load(f"{job_name}: {processed}")
-
-
+    load(validated)
+ 
+    load(f"{job_name}: {processed}")
+ 
 @flow
 def post_etl_flow():
     return "Post ETL tasks executed"  # Logs not visible
-
+ 
 @flow
 def notification_flow():
     return "Notification sent"  # Logs not visible
