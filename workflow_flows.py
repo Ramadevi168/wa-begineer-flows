f990c05e9c6e6a6f2ea7c0f994a6ef706fdb372b
Ramadevi168 <samula.ramadevi@ibm.com>
2025-12-15 17:03:55 +0530

    Update workflow_flows.py

--- workflow_flows.py
+++ workflow_flows.py
@@ -9,20 +9,22 @@ def transform(data):
     return f"{data} → Transformed"
 
 @task
+def validate(data):
+    return f"{data} → Validated"
+
+@task
 def load(data):
     return f"{data}"  # Logs will not show in Cloud
 
-@task
-def validate(data):
-    return f"{data} → Validated"
 
 @flow
 def etl_flow(job_name: str = "Daily ETL"):
     raw = extract()
 	processed = transform(raw)
-	load(f"{job_name}: {processed}")
     validated = validate(processed)
 	load(validated)
+
+	load(f"{job_name}: {processed}")
 
 
 @flow
